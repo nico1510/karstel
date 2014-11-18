c0f2d9b73a3731a000af4b69ad262c79751adbc0
nico1510 <Nicolas-Beck@gmx.de>
2014-11-18 11:12:57 +0100

    project almost completely finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+"use strict";
+
+function calendarClosure(newId) {
+  var visible;
+  var clone = $('#calendar-template').clone().attr('id', newId);
+  clone.insertBefore($('#calendar-template'));
+  return function toggleListener(ev) {
+    visible = !visible;
+    clone.css({
+      position : 'absolute',
+      left: $(this).offset().left + $(this).outerWidth()+'px',
+      top: $(this).offset().top +'px',
+      display : visible ? 'block' : 'none'
+    });
+  };
+}
+
+$('#start-calendar-button').on('click', calendarClosure('start-calendar-content'));
+$('#end-calendar-button').on('click', calendarClosure('end-calendar-content'));
