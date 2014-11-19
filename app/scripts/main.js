387b36d265c881b7b7f0cd5a62b18ed10d069e12
nico1510 <Nicolas-Beck@gmx.de>
2014-11-19 23:12:17 +0100

    looks more like a calendar now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,12 @@
 "use strict";
 
+moment.locale('de');
 
 function daysInMonthToHtml(date) {
   var htmlString = '';
-  for (var w = moment(date.startOf('month')); w.isBefore(date.endOf('month')); w.add(1, 'weeks')) {
+  for (var w = moment(date.startOf('month')); w <= date.endOf('month'); w.add(1, 'weeks')) {
     htmlString += '<tr>';
-    for(var d = moment(w.startOf('week')); d.isBefore(w.endOf('week')); d.add(1, 'days')) {
+    for (var d = moment(w.startOf('isoWeek')); d <= w.endOf('isoWeek'); d.add(1, 'days')) {
       htmlString += '<td>' + d.format('DD') + '</td>';
     }
     htmlString += '</tr>';
@@ -13,24 +14,36 @@ function daysInMonthToHtml(date) {
   return htmlString;
 }
 
+function generateCalendarHeader(date) {
+  var weekDays = moment.weekdaysMin();
+  var sunday = weekDays.shift();
+  weekDays.push(sunday);
+  var htmlString = '<thead><tr>';
+  moment.weekdaysMin().forEach(function (dayName) {
+    htmlString += '<td>' + dayName + '</td>';
+  });
+  htmlString += '</tr></thead>';
+  return htmlString;
+}
+
 function calendarClosure(newId) {
   var clone = $('#calendar-template').clone().attr('id', newId);
   clone.insertBefore($('#calendar-template'));
   var date = moment();
-  $('#' + newId + ' .calendar-content .table' ).html(daysInMonthToHtml(date));
+  $('#' + newId + ' .calendar-content .table').html(generateCalendarHeader(date) + daysInMonthToHtml(date));
 
   return function toggleListener(ev) {
-      clone.css({
-        position: 'absolute',
-        left: $(this).offset().left + $(this).outerWidth() + 'px',
-        top: $(this).offset().top + (0.5* $(this).outerHeight()) - (0.5 * clone.outerHeight()) + 'px',
-        display: 'block'
-      });
+    clone.css({
+      position: 'absolute',
+      left: $(this).offset().left + $(this).outerWidth() + 'px',
+      top: $(this).offset().top + (0.5 * $(this).outerHeight()) - (0.5 * clone.outerHeight()) + 'px',
+      display: 'block'
+    });
   };
 }
 
 function hideCalendar(id) {
-  return function() {
+  return function () {
     $('#' + id).css('display', 'none');
   };
 }
@@ -41,3 +54,14 @@ $('#end-calendar-button').on('click', calendarClosure('end-calendar-content'));
 
 $('#start-calendar-button').on('blur', hideCalendar('start-calendar-content'));
 $('#end-calendar-button').on('blur', hideCalendar('end-calendar-content'));
+
+// calendar header labels
+$('#start-calendar-content > h3').html('Anreisedatum');
+$('#end-calendar-content > h3').html('Abreisedatum');
+
+// hovering effect of table cells
+$('table.calendar-table > tbody > tr > td').hover(function () {
+  $(this).css('background-color', '#D9EDF7');
+}, function () {
+  $(this).css('background-color', '#FFFFFF');
+});
