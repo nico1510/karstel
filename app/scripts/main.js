3eb61592baa9be8a85e77b8dbfa8818919badead
nico1510 <Nicolas-Beck@gmx.de>
2014-11-20 10:43:05 +0100

    refactoring / created Calendar Object

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,67 +2,70 @@
 
 moment.locale('de');
 
-function daysInMonthToHtml(date) {
-  var htmlString = '';
-  for (var w = moment(date.startOf('month')); w <= date.endOf('month'); w.add(1, 'weeks')) {
-    htmlString += '<tr>';
-    for (var d = moment(w.startOf('isoWeek')); d <= w.endOf('isoWeek'); d.add(1, 'days')) {
-      htmlString += '<td>' + d.format('DD') + '</td>';
-    }
-    htmlString += '</tr>';
-  }
-  return htmlString;
-}
 
-function generateCalendarHeader(date) {
-  var weekDays = moment.weekdaysMin();
-  var sunday = weekDays.shift();
-  weekDays.push(sunday);
+function KarstelCalendar(trigger, id, header, daySelectCallback) {
 
-  var htmlString = '<thead><tr>';
-  weekDays.forEach(function (dayName) {
-    htmlString += '<td>' + dayName + '</td>';
-  });
-  htmlString += '</tr></thead>';
-  return htmlString;
-}
-
-function calendarClosure(newId) {
-  var clone = $('#calendar-template').clone().attr('id', newId);
-  clone.insertBefore($('#calendar-template'));
   var date = moment();
-  $('#' + newId + ' .calendar-content .table').html(generateCalendarHeader(date) + daysInMonthToHtml(date));
 
-  return function toggleListener(ev) {
-    clone.css({
+  (function create() {
+    var clone = $('#calendar-template').clone().attr('id', id);
+    clone.find('h3').html(header);
+    clone.find('.calendar-content .table').html(generateCalendarHeader() + daysInMonthToHtml(date));
+    clone.appendTo('body');
+  })();
+
+  function showCalendar(ev) {
+    $('#' + id).css({
       position: 'absolute',
-      left: $(this).offset().left + $(this).outerWidth() + 'px',
-      top: $(this).offset().top + (0.5 * $(this).outerHeight()) - (0.5 * clone.outerHeight()) + 'px',
-      display: 'block'
+      left: trigger.offset().left + trigger.outerWidth() + 'px',
+      top: trigger.offset().top + (0.5 * trigger.outerHeight()) - (0.5 * $('#' + id).outerHeight()) + 'px',
+      display : 'block'
     });
-  };
-}
+  }
 
-function hideCalendar(id) {
-  return function () {
+  function hideCalendar(ev) {
     $('#' + id).css('display', 'none');
-  };
+  }
+
+  function daysInMonthToHtml(date) {
+    var htmlString = '';
+    for (var w = moment(date.startOf('month')); w <= date.endOf('month'); w.add(1, 'weeks')) {
+      htmlString += '<tr>';
+      for (var d = moment(w.startOf('isoWeek')); d <= w.endOf('isoWeek'); d.add(1, 'days')) {
+        htmlString += '<td>' + d.format('DD') + '</td>';
+      }
+      htmlString += '</tr>';
+    }
+    return htmlString;
+  }
+
+  function generateCalendarHeader() {
+    var weekDays = moment.weekdaysMin();
+    var sunday = weekDays.shift();
+    weekDays.push(sunday);
+
+    var htmlString = '<thead><tr>';
+    weekDays.forEach(function (dayName) {
+      htmlString += '<td>' + dayName + '</td>';
+    });
+    htmlString += '</tr></thead>';
+    return htmlString;
+  }
+
+  trigger.on('click', showCalendar);
+  trigger.on('blur', hideCalendar);
+
+  // hovering effect of table cells
+  $('#' + id + ' .calendar-content .table > tbody > tr > td').hover(function () {
+    $(this).css('background-color', '#D9EDF7');
+  }, function () {
+    $(this).css('background-color', '#FFFFFF');
+  });
 }
 
 
-$('#start-calendar-button').on('click', calendarClosure('start-calendar-content'));
-$('#end-calendar-button').on('click', calendarClosure('end-calendar-content'));
 
-$('#start-calendar-button').on('blur', hideCalendar('start-calendar-content'));
-$('#end-calendar-button').on('blur', hideCalendar('end-calendar-content'));
+var startCal = new KarstelCalendar($('#start-calendar-button'), 'start-calendar-content', 'Anreisedatum');
+var endCal = new KarstelCalendar($('#end-calendar-button'), 'end-calendar-content', 'Abreisedatum');
 
-// calendar header labels
-$('#start-calendar-content > h3').html('Anreisedatum');
-$('#end-calendar-content > h3').html('Abreisedatum');
 
-// hovering effect of table cells
-$('table.calendar-table > tbody > tr > td').hover(function () {
-  $(this).css('background-color', '#D9EDF7');
-}, function () {
-  $(this).css('background-color', '#FFFFFF');
-});
