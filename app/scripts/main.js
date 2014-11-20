cfc2b18456a618134d85f0185bbcb5649a653b53
nico1510 <Nicolas-Beck@gmx.de>
2014-11-20 21:33:41 +0100

    added day select callback

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,7 +10,7 @@ function KarstelCalendar(trigger, id, header, daySelectCallback) {
   (function create() {
     var clone = $('#calendar-template').clone().attr('id', id);
     clone.find('h3').html(header);
-    clone.find('.calendar-content .table').html(generateCalendarHeader() + daysInMonthToHtml(date));
+    clone.find('.calendar-content .table').append(generateCalendarHeader()).append(generateCalendarCells(date));
     clone.appendTo('body');
   })();
 
@@ -24,48 +24,61 @@ function KarstelCalendar(trigger, id, header, daySelectCallback) {
   }
 
   function hideCalendar(ev) {
-    $('#' + id).css('display', 'none');
+      $('#' + id).css('display', 'none');
   }
 
-  function daysInMonthToHtml(date) {
-    var htmlString = '';
+  function generateCalendarCells(date) {
+    var tbody = $('<tbody></tbody>');
+    var trow;
+    var cell;
+    var hoverIn = function () {
+      $(this).css('background-color', '#D9EDF7');
+    };
+    var hoverOut = function () {
+      $(this).css('background-color', '#FFFFFF');
+    };
+    var onDaySelect = function(d) {
+      return function() {
+        daySelectCallback(d);
+      };
+    };
+
     for (var w = moment(date.startOf('month')); w <= date.endOf('month'); w.add(1, 'weeks')) {
-      htmlString += '<tr>';
+      trow = $('<tr></tr>');
       for (var d = moment(w.startOf('isoWeek')); d <= w.endOf('isoWeek'); d.add(1, 'days')) {
-        htmlString += '<td>' + d.format('DD') + '</td>';
+        cell = $('<td></td>').html(d.format('DD'));
+        cell.hover(hoverIn,hoverOut);
+        cell.mousedown(onDaySelect(moment(d)));
+        trow.append(cell);
       }
-      htmlString += '</tr>';
+      tbody.append(trow);
     }
-    return htmlString;
+    return tbody;
   }
 
   function generateCalendarHeader() {
     var weekDays = moment.weekdaysMin();
     var sunday = weekDays.shift();
     weekDays.push(sunday);
-
-    var htmlString = '<thead><tr>';
+    var tableHeader = $('<thead><tr></tr></thead>');
+    var tableCell;
     weekDays.forEach(function (dayName) {
-      htmlString += '<td>' + dayName + '</td>';
+      tableCell = $('<td></td>').html(dayName);
+      tableHeader.find('tr').append(tableCell);
     });
-    htmlString += '</tr></thead>';
-    return htmlString;
+
+    return tableHeader;
   }
 
-  trigger.on('click', showCalendar);
-  trigger.on('blur', hideCalendar);
-
-  // hovering effect of table cells
-  $('#' + id + ' .calendar-content .table > tbody > tr > td').hover(function () {
-    $(this).css('background-color', '#D9EDF7');
-  }, function () {
-    $(this).css('background-color', '#FFFFFF');
-  });
+  trigger.click(showCalendar);
+  trigger.blur(hideCalendar);
 }
 
-
-
-var startCal = new KarstelCalendar($('#start-calendar-button'), 'start-calendar-content', 'Anreisedatum');
-var endCal = new KarstelCalendar($('#end-calendar-button'), 'end-calendar-content', 'Abreisedatum');
+var startCal = new KarstelCalendar($('#start-calendar-button'), 'start-calendar-content', 'Anreisedatum', function onDaySelect(d) {
+  console.log(d.format('LLLL'));
+});
+var endCal = new KarstelCalendar($('#end-calendar-button'), 'end-calendar-content', 'Abreisedatum', function onDaySelect(d) {
+  console.log(d.format('LLLL'));
+});
 
 
